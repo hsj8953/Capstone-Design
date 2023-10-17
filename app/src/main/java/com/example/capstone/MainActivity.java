5aed4be07f2da8af8732aaa800fde8928823ce72
tpwls8953 <tpwls8953@gmail.com>
2023-10-17 11:02:52 +0900

    하단 네비게이션 코드 수정

--- app/src/main/java/com/example/capstone/MainActivity.java
+++ app/src/main/java/com/example/capstone/MainActivity.java
@@ -20,8 +20,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
-        getSupportActionBar().hide();
-        getSupportActionBar().hide();
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
