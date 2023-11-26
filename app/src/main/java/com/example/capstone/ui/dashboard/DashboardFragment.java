87eb4fd398cfefe049273ca5d5b2a4eed66ba411
ZeroJ1nZero <wlsdudrhd@gmail.com>
2023-11-26 22:36:22 +0900

    11/27 서버랑 연결 후 추가적인 작업 이루어질것같습니다

--- app/src/main/java/com/example/capstone/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/capstone/ui/dashboard/DashboardFragment.java
@@ -1,12 +1,15 @@
 package com.example.capstone.ui.dashboard;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -16,14 +19,6 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.capstone.R;
-import com.example.capstone.databinding.FragmentDashboardBinding;
-import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.libraries.places.api.Places;
-import com.google.android.libraries.places.api.model.Place;
-import com.google.android.libraries.places.api.net.FetchPlaceRequest;
-import com.google.android.libraries.places.api.net.PlacesClient;
-
 import org.w3c.dom.Text;
 
 import java.util.ArrayList;
@@ -31,118 +26,246 @@ import java.util.Arrays;
 import java.util.List;
 
 public class DashboardFragment extends Fragment {
-    private PlacesClient placesClient;
-    private static final String API_KEY = "AIzaSyBJrumY0iDABQtCBlwS2jtabIeU80M6hk4";
     private LinearLayout placeLayout, countryLayout;
     private TextView noPlace;
-//    private List<String> country = new ArrayList<>();
-    //DB 연결 필수
-//    private List<Integer> days = new ArrayList<>();
-    //DB 연결 필수
+    private static List<String> country = new ArrayList<>();
+    //DB 연결
+    private static List<Integer> days = new ArrayList<>();
+    //DB 연결
 //    private List<String> locations = new ArrayList<>();
-    // 각 나라/일자별로 장소를 저장해서 DB로부터 장소 목록을 받아서 버튼을 추가
-    // 장소를 받는 메서드는 아직 구현하지 않음
-    // 장소 추가 삭제 메서드 구현
-    // 나라가 추가됐을때 플러스 버튼 왼쪽에 나라탭이 추가되도록 할것
-    private Button plus;
+    ArrayList<ArrayList<String>> locations = new ArrayList<>();
+    // 각 나라/일자별로 장소를 저장해서 서버로부터 장소 목록을 받아서 버튼을 추가
+    // 장소 삭제 메서드 구현
+//    private Button plus;
     private Integer[] backgroundcolor = {R.color.purple_200, com.google.android.libraries.places.R.color.quantum_brown, com.google.android.libraries.places.R.color.quantum_lightblue};
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_dashboard, container, false);
 
-        //장소에 대한 정보를 가져오는 메서드
-        Places.initialize(getContext(), API_KEY);
-        placesClient = Places.createClient(requireContext());
-        getPlaceDetails("");
-
         placeLayout = view.findViewById(R.id.place_layout);
         countryLayout = view.findViewById(R.id.country_layout);
-        plus = view.findViewById(R.id.plus);
+//        plus = view.findViewById(R.id.plus);
         noPlace = view.findViewById(R.id.no_place);
+
+        ButtonDynamically();
+
         //나라 추가 버튼
-        plus.setOnClickListener(new View.OnClickListener() {
+//        plus.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                addCountry();
+//            }
+//        });
+
+        return view;
+    }
+
+    private void addCountry() {
+        showCustomLayoutPopup(requireContext());
+        //서버에 전송하는 메서드
+    }
+
+    //서버에 객체를 전달할때 메서드가 호출되도록 설정해야함
+    //나라 탭을 구성하는 메서드
+    private void ButtonDynamically() {
+        if (locations.size() != -1) {
+            for (int i = 0; i < country.size(); i++) {
+                noPlace.setVisibility(View.GONE);
+                Button button = new Button(requireContext());
+                button.setText(country.get(i));
+//            callPlan(country.size() - 1);
+
+                button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        int countryIndex = country.indexOf(button.getText());
+                        callPlan(countryIndex);
+                    }
+                });
+                button.setBackgroundColor(backgroundcolor[(country.size() - 1) % backgroundcolor.length]);
+                button.setHeight(80);
+                button.setWidth(60);
+                button.setBackgroundResource(R.drawable.search_button);
+                countryLayout.addView(button);
+            }
+        } else noPlace.setVisibility(View.VISIBLE);
+
+        //plus버튼 생성
+        Button button = new Button(requireContext());
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
                 addCountry();
             }
         });
 
-        return view;
-    }
-    private void getPlaceDetails(String placeId) {
-        // Place Details 요청
-        List<Place.Field> placeFields = Arrays.asList(Place.Field.ID, Place.Field.NAME, Place.Field.ADDRESS, Place.Field.PHONE_NUMBER);
-
-        FetchPlaceRequest request = FetchPlaceRequest.builder(placeId, placeFields).build();
-
-        placesClient.fetchPlace(request)
-                .addOnSuccessListener((response) -> {
-                    Place place = response.getPlace();
-                    Log.i("PlaceDetails", "Place found: " + place.getName());
-
-                    // 원하는 정보를 사용하도록 수정
-                    String name = place.getName();
-                    String address = place.getAddress();
-                    String phoneNumber = place.getPhoneNumber();
-
-                    // 이후 작업 수행
-                })
-                .addOnFailureListener((exception) -> {
-                    if (exception instanceof ApiException) {
-                        ApiException apiException = (ApiException) exception;
-                        int statusCode = apiException.getStatusCode();
-                        Log.e("PlaceDetails", "Place not found: " + exception.getMessage());
-                    }
-                });
+        //plus버튼 디자인 부분
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+        layoutParams.width = 210;
+        layoutParams.height = 180;
+        button.setLayoutParams(layoutParams);
+        button.setBackgroundResource(R.drawable.plus_btn);
+        countryLayout.addView(button);
     }
 
+    //나라 탭의 이름에 따라 맞는 일자와 장소를 표시해주는 메서드
+    private void callPlan(int index) {
+        for (int i = 1; i <= days.get(index); i++) {
+            Button button = new Button(requireContext());
 
-    private void addCountry() {
-        //팝업창을 띄우면서 나라 입력하고 일자를 받는 메서드 추가해야함
-        //나라는 country.add() 일자는 days.add()
-//        ButtonDynamically();
+            //일자 탭의 디자인 부분
+            button.setText(i + "일차");
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT);
+            layoutParams.width = 1000;
+            layoutParams.height = 180;
+            button.setLayoutParams(layoutParams);
+            button.setBackgroundResource(R.drawable.search_button);
+
+            //일자 버튼을 눌렀을때 나오는 메서드
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    showCustomSearchpop(requireContext());
+                }
+            });
+
+            //textView 추가
+            placeLayout.addView(button);
+
+            // 첫 번째 행 추가 예시
+            // days에 의존해서 일자가 제한적으로 나오도록 해야함
+            // 아마 한다면 전에 있던 ArrayList를 pop해서 새로운 배열을 만든 다음에 장소를 추가해서 새로운 ArrayList를 만들어 주는 거
+            ArrayList<String> row1 = new ArrayList<>();
+            row1.add("서울");
+            row1.add("경기도");
+            row1.add("부산");
+            locations.add(row1);
+
+            //2차원 배열 locations의 일자 순서대로 장소를 추가 해주는 메서드
+            for (String value : locations.get(i - 1)) {
+                TextView textView = new TextView(requireContext());
+
+                //장소 버튼의 디자인 부분
+                textView.setText(value);
+                layoutParams.width = 900;
+                layoutParams.height = 150;
+                button.setLayoutParams(layoutParams);
+                button.setBackgroundResource(R.drawable.search_button);
+
+                String details = value;
+                textView.setText(details);
+                placeLayout.addView(textView);
+            }
+        }
     }
 
-//    private void ButtonDynamically() {
-//        if (locations.size() != -1) {
-//            noPlace.setVisibility(View.GONE);
-//            Button button = new Button(requireContext());
-//            button.setText(country.get(country.size() - 1));
-//            callPlan(country.size() - 1);
-//            button.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View view) {
-//                    int countryIndex = country.indexOf(button.getText());
-//                    callPlan(countryIndex);
-//                }
-//            });
-//            button.setBackgroundColor(backgroundcolor[(country.size() - 1) % backgroundcolor.length]);
-//            button.setHeight(80);
-//            button.setWidth(60);
-//            button.setBackgroundResource(R.drawable.search_button);
-//            countryLayout.addView(button);
-//        } else noPlace.setVisibility(View.VISIBLE);
-//    }
+    private static void showCustomSearchpop(Context context) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View customLayout = inflater.inflate(R.layout.custom_search_layout, null);
 
-//    private void callPlan(int index) {
-//        for (int i = 1; i <= days.get(index); i++) {
-//            TextView textView = new TextView(requireContext());
-//            textView.setText(i + "일차");
-//            textView.setWidth(360);
-//            textView.setHeight(60);
-//            textView.setBackgroundResource(R.drawable.search_button);
-//            placeLayout.addView(textView);
-//            for (int j = 0; j < locations.size(); j++) {
-//                Button button = new Button(requireContext());
-//                button.setText(locations.get(j));
-//                button.setOnClickListener(new View.OnClickListener() {
-//                    @Override
-//                    public void onClick(View view) {
-//                        //장소이름에 따라 장소정보를 가져와 표시해주는 페이지를 보여줌
-//                    }
-//                });
-//                placeLayout.addView(button);
+        // 레이아웃 내의 View들 참조
+        TextView titleTextView = customLayout.findViewById(R.id.titleTextView);
+        EditText editText1 = customLayout.findViewById(R.id.editText1);
+
+        titleTextView.setText("장소 검색"); // 제목 설정
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setView(customLayout);
+
+        builder.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // 확인 버튼을 클릭할 때 수행할 동작
+                String value1 = editText1.getText().toString();
+
+                //검색할때 넘겨야할 장소이름 value1
+            }
+        });
+
+        builder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // 취소 버튼을 클릭할 때 수행할 동작
+                dialog.cancel(); // 팝업창 닫기
+            }
+        });
+
+        // 팝업창 생성 및 표시
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    //나라 추가 팝업창을 띄우는 메서드
+    public static void showCustomLayoutPopup(final Context context) {
+        // 레이아웃 인플레이션
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View customLayout = inflater.inflate(R.layout.custom_popup_layout, null);
+
+        // 레이아웃 내의 View들 참조
+        TextView titleTextView = customLayout.findViewById(R.id.titleTextView);
+        EditText editText1 = customLayout.findViewById(R.id.editText1);
+        EditText editText2 = customLayout.findViewById(R.id.editText2);
+
+        titleTextView.setText("나라를 추가 해 주세요"); // 제목 설정
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setView(customLayout);
+
+        builder.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // 확인 버튼을 클릭할 때 수행할 동작
+                String value1 = editText1.getText().toString();
+                String value2 = editText2.getText().toString();
+
+                country.add(value1);
+                days.add(Integer.valueOf(value2));
+            }
+        });
+
+        builder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // 취소 버튼을 클릭할 때 수행할 동작
+                dialog.cancel(); // 팝업창 닫기
+            }
+        });
+
+        // 팝업창 생성 및 표시
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+    //장소이름에 따라 장소정보를 가져와 표시해주는 페이지를 보여주는 메서드
+    //검색어에 따라 장소 정보를 가져오는 메서드로 교체 예정
+//    private void getPlaceDetails(String placeId) {
+//        // Place Details 요청
+//        List<Place.Field> placeFields = Arrays.asList(Place.Field.ID, Place.Field.NAME, Place.Field.ADDRESS, Place.Field.PHONE_NUMBER);
+//        FetchPlaceRequest request = FetchPlaceRequest.builder(placeId, placeFields).build();
+//
+//        placesClient.fetchPlace(request)
+//                //요청이 성공하였을 경우 호출되는 리스너
+//                .addOnSuccessListener((response) -> {
+//            Place place = response.getPlace();
+//            Log.i("PlaceDetails", "Place found: " + place.getName());
+//
+//            // 원하는 정보를 사용하도록 수정
+//            String name = place.getName();
+//            String address = place.getAddress();
+//            String phoneNumber = place.getPhoneNumber();
+//
+//            //
+//        })
+//                //요청이 실패하였을 경우 호출되는 리스너
+//                .addOnFailureListener((exception) -> {
+//            if (exception instanceof ApiException) {
+//                ApiException apiException = (ApiException) exception;
+//                int statusCode = apiException.getStatusCode();
+//                Log.e("PlaceDetails", "Place not found: " + exception.getMessage());
 //            }
-//        }
+//        });
 //    }
 }
